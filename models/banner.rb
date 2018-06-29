79d6ac6dda29ff6e5d52e1070ea02e03f28e96c2
AleshaRP <createlele@yahoo.com>
2018-06-29 12:42:41 +0000

    automatically backed up by learn

---
+++ models/banner.rb
@@ -0,0 +1,17 @@
+def banner_rotation
+   cycle = 0
+   loop do
+   if cycle <= 10
+     puts "The five most polluted places in the world include (Gwalior, India), (Allahabad,India), (Al Jubail, Saudi Arabia), (Pasakha, Bhuta), (Raipur, India) "
+     cycle += 1
+    elsif  cycle <= 20
+     puts "2.12 billion tons of waste is produced globally each year."
+     cycle += 1
+   elsif cycle <= 30
+	puts “Hazardous air pollutants also called air toxic's include 187 pollutants.”
+	Cycle += 1
+   else
+     cycle =0
+   end
+  end
+ end
---
+++ models/questions.rb
@@ -0,0 +1,9 @@
+def environment_chooser(user_value)
+	if user_value < 3
+		"ameteur"
+	elsif user_value < 5
+		"aware"
+	else
+		"crusader"
+	end
+end
