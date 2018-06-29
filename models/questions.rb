55cbd16acd1fcc8a50e3ab6def7ab97d90c0268a
AleshaRP <createlele@yahoo.com>
2018-06-29 13:52:00 +0000

    automatically backed up by learn

--- models/questions.rb
+++ models/questions.rb
@@ -1,9 +1,9 @@
 def environment_chooser(user_value)
-	if user_value < 3
+	if user_value <= 3
 		"ameteur"
-	elsif user_value < 5
+	elsif user_value <= 5
 		"aware"
-	elsif user_value < 10
+	elsif user_value <= 10
 		"crusader"
 	end
 end
